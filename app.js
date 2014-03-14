0a80df0027a0d3c59422004354234cdc7a8ccd05
Junhee Woo <dyoona@gmail.com>
2014-03-14 18:35:41 +0900

    Writing file data1.txt with nested async

--- app.js
+++ app.js
@@ -4,18 +4,35 @@ var fs = require('fs'),
 try {
     async.parallel({
         data1: function(callback) {
-                   fs.readFile('./data/data1.txt', 'utf8', function(err, data) {
-                       callback(err,data);
+                   async.waterfall([
+                       function readFile(callback) {
+                           fs.readFile('./data/data1.txt', 'utf8', function(err, data) {
+                               callback(err, data);
+                           });
+                       },
+                       function modify(text, callback) {
+                           var adjdata = text.replace(/./, 'K');
+                           callback(null, adjdata);
+                       },
+                       function writeData(data, callback) {
+                           fs.writeFile('./data/data1.txt', data, function(err) {
+                               callback(err, './data/data1.txt');
+                           });
+                       }
+                   ], function(err, result) {
+                       if(err) throw err;
+                       console.log('modified ' + result);
+                       callback(err, 'modified ' + result);
                    });
                },
         data2: function(callback) {
                    fs.readFile('./data/data2.txt', 'utf8', function(err, data) {
-                       callback(err,data);
+                       callback(err, data);
                    });
                },
         data3: function(callback) {
                    fs.readFile('./data/data3.txt', 'utf8', function(err, data) {
-                       callback(err,data);
+                       callback(err, data);
                    });
                },
     }, function(err, result) {
